aab7b704c8768af6d6ec2bea9d119e4e5859341c
wonedd <vinmarv@hotmail.com>
2022-09-14 15:03:20 -0300

    waiting solid lessons

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import { categoriesRoutes } from 'routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/categories', categoriesRoutes);
+
 app.listen(3333, () => {
+  // eslint-disable-next-line no-console
   console.log('Server started on port 3333!');
 });
